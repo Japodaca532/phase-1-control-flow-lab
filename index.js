37d1f52bd8d4057bf9d630ecb27ff21ff237d7c9
Jacqueline Apodaca <jacapodaca42@gmail.com>
2023-03-18 20:32:09 -0600

    completedwork.md

--- index.js
+++ index.js
@@ -12,10 +12,14 @@ else (rideLength > 2500);{
   return 'No can do.'
 }
 }
-function ternaryCheckCity(NYC){
+function ternaryCheckCity(city){
+  if (city = NYC ){
  return 'Ok, sounds good.'
+} 
+else if (city != NYC){
+  return 'No go'
 }
-
+}
 function switchOnCharmFromTip(generousTip){
  return 'Thank you so much.'
 }
