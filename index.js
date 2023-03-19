9161c43c07f60d3ee68ef1d5ea3e75529ad69dd2
Jacqueline Apodaca <jacapodaca42@gmail.com>
2023-03-18 19:31:47 -0600

    completedwork.md

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ function scuberGreetingForFeet(rideLength){
   if (rideLength <=400){
   return 'This one is on me!'
 }
-else if (rideLength >400 && rideLenght <=2000){
+else if (rideLength >400 && rideLength <=2000){
   return 'That will be twenty bucks.'
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+else if (rideLength > 2000 && rideLength <=2500){
+  return 'I will gladly take your thirty bucks.'
+}
+else (rideLength > 2500);{
+  return 'No can do.'
+}
+}
+function ternaryCheckCity(city){
+  const result = city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
-} }
+} 
